f8f6d1fd9de9b88bc4110fa47af5780df9097b43
hrrs01 <haavard.hn@gmail.com>
2022-11-17 14:09:41 +0100

    Trying a new file structure

--- src/main.rs
+++ src/main.rs
@@ -3,47 +3,57 @@ use minifb::*;
 pub mod helper;
 use helper::*;
 
-const WIDTH: usize = 800;
-const HEIGHT: usize = 480;
-
-
 fn main(){
-    let mut buffer: Buffer = Buffer::new(WIDTH as u32, HEIGHT as u32);
+    
     //let mut buffer: Vec<u32> = vec![];
-    let win_ops = WindowOptions {
-        borderless: true,
-        ..Default::default()
-    };
-
-    // for actually displaying stuff on windows, and in a desktop enviroment
-    let mut window = Window::new(
-        "AR23",
-        WIDTH,
-        HEIGHT,
-        win_ops
-        
-    ).unwrap_or_else(|e|{
-        panic!("{}",e);
-    });
-
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
-
-
-    let p1 = Point { x: 300, y: 300 };
-    let p2 = Point { x: 500, y: 200 };
-
-    let mut txt = Text::new(&mut buffer, p1, "Hello, World", 40.0, Color::RED);
-
-    let mut i: u16 = 0;
-
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        i += 1;
-        buffer.clear();
-        txt.update_text(&buffer, i.to_string().as_str());
-        txt.draw(&mut buffer);
-        window.update_with_buffer(&buffer.to_buffer(), WIDTH, HEIGHT).unwrap();
-    }
+    let mut gui = AR23GUI::new(800, 480);
 
 }
+
+
+struct AR23GUI {
+    window: Window, 
+    width: u32,
+    height: u32,
+    screens: Vec<AR23Screen>,
+    screen_index: usize
+}
+
+impl AR23GUI {
+    fn new(width: u32, height: u32) -> Self {
+        let options = WindowOptions {
+            borderless: true, 
+            ..Default::default()
+        };
+        let mut window = Window::new("AR23 GUI", width as usize, height as usize, options).unwrap();
+        AR23GUI { window: window, width: width, height: height, screens: Vec::new(), screen_index: 0 }
+    }
+
+    fn append_screen(&mut self, screen: AR23Screen) {
+        self.screens.push(screen);
+    }
+
+    fn draw_screen(&mut self) -> &Buffer {
+        if 0 >= self.screens.len() {
+            panic!("No screens added!");
+            return &Buffer::new(self.width, self.height);
+        }
+        
+        return &self.screens[self.screen_index].buffer;
+    }
+}
+
+struct AR23Screen {
+    buffer: Buffer
+}
+
+impl AR23Screen {
+    fn new(b: Buffer) -> Self {
+        AR23Screen {
+            buffer: b
+        }
+    }
+}
+
 
 
